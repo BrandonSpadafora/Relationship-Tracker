4c6a18c6ece7f1f84fdf2810ee28609ed7497b5f
Brandon Spadafora <brandon.spadafora01@gmail.com>
2023-12-23 07:23:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ with col1:
                 <path class="heart" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
             </svg>
             <!-- Embed your anniversary information as a separate text element -->
-            <text x="50%" y="65%" text-anchor="middle" dominant-baseline="middle" class="heart-text">Anniversary: June 22, 2022</text>
+            <text x="50%" y="65%" text-anchor="middle" dominant-baseline="middle" class="heart-text">Anniversary: June 22</text>
         </div>
         """,
         unsafe_allow_html=True,
